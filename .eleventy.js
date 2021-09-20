e174af3d568d9aa074eddbde1cf7939e713ed495
Holben888 <bholmesdev@gmail.com>
2021-09-20 10:12:56 -0400

    add _includes passthrough copy

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('assets')
+  eleventyConfig.addPassthroughCopy('_includes/components')
 
   eleventyConfig.addShortcode('react', function() {
     return 'it worked!'
