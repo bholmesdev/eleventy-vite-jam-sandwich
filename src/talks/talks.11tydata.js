de0ee891f9a14386678592093c8354b03902276f
Holben888 <bholmesdev@gmail.com>
2021-10-07 23:00:02 -0400

    feat: fetch GH avatar by username as picture backup

--- src/talks/talks.11tydata.js
+++ src/talks/talks.11tydata.js
@@ -1,4 +1,17 @@
+const fetch = require('node-fetch')
+
 module.exports = {
   layout: 'talk-layout',
   tags: 'talks',
+  eleventyComputed: {
+    picture: async ({ picture, speakers }) => {
+      if (!picture && speakers?.[0]?.ghUsername) {
+        const result = await fetch(`https://api.github.com/users/${speakers[0].ghUsername}`)
+        const { avatar_url = '' } = await result.json()
+        return avatar_url
+      } else {
+        return picture
+      }
+    },
+  },
 }
